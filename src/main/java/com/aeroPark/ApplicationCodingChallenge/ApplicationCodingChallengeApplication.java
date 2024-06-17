4c836b4b942b61c86ba6286de1a4036950a6fe05
Gabriel Wright <gabemagwright@gmail.com>
2024-06-17 18:08:23 +0100

    Initial commit

---
+++ src/main/java/com/aeroPark/ApplicationCodingChallenge/ApplicationCodingChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.aeroPark.ApplicationCodingChallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApplicationCodingChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApplicationCodingChallengeApplication.class, args);
+	}
+
+}
